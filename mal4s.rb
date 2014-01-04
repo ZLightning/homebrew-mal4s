61acdd6b3f775c8fc4cc6f04103a962d459acf9e
Zigmund Ozea <ziggy@dissectcyber.com>
2014-01-04 17:43:22 -0500

    Updated to match pull request.

--- mal4s.rb
+++ mal4s.rb
@@ -1,17 +1,17 @@
 require 'formula'
 
 class Mal4s < Formula
-
   homepage 'https://github.com/secure411dotorg/mal4s/'
   url 'https://service.dissectcyber.com/mal4s/mal4s-1.1.0.tar.gz'
   sha1 '6431784384b11deaf4448b7f94bbb6eb4877a66e'
 
   head 'https://github.com/secure411dotorg/mal4s.git'
 
+  depends_on :macos => :mavericks
   depends_on :automake
   depends_on :autoconf
   depends_on :libtool
-  depends_on :x11 if build.with? 'x'
+  depends_on :x11 => :optional
   depends_on :freetype
   depends_on 'pkg-config' => :build
   depends_on 'glm' => :build
@@ -26,9 +26,13 @@ class Mal4s < Formula
   def install
     args = ["--disable-dependency-tracking",
             "--prefix=#{prefix}"]
-    args << "--without-x" unless build.with? 'x'
+    args << "--without-x" unless build.with? 'x11'
     system "autoreconf", "-f", "-i"
     system "./configure", *args
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mal4s", "--help"
   end
 end
